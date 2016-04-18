6c912d3be76abaec25f2ab94bed52e7dd9b3ee70
cheren <kalistratov.s.m@gmail.com>
2016-04-18 12:14:39 +0300

    Widget inheritance through one

---
+++ tests/assets/js/widget.color.inherit.custom.js
@@ -0,0 +1,17 @@
+/**
+ * JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+ *
+ * @package     jbzoo
+ * @version     2.x Pro
+ * @author      JBZoo App http://jbzoo.com
+ * @copyright   Copyright (C) JBZoo.com,  All rights reserved.
+ * @license     http://jbzoo.com/license JBZoo Licence
+ */
+
+
+;
+(function ($, window, document, undefined) {
+
+    JBZoo.widget('JBZoo.Colors.Inherit.Custom', {}, {});
+
+})(jQuery, window, document);
